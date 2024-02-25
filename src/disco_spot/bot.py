c72b12bbd5c603e22d43c5cc6b79e98f3680c17b
BrianCechmanek <bcechmanek@gmail.com>
2024-02-25 01:48:55 +0000

    now with classes, pydantic, and (some) tests!

--- src/disco_spot/bot.py
+++ src/disco_spot/bot.py
@@ -4,14 +4,14 @@
 from dotenv import load_dotenv
 import logging
 import logging.handlers
-import os
+from pydantic_settings import BaseSettings
 import re
 import sys
 
 import discord
+from discord.ext import commands
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import spotipy
-
 from pyyoutube import Api   # https://console.cloud.google.com/apis/credentials?)
 
 
@@ -30,124 +30,142 @@ logger.addHandler(handler)
 
 logging.debug("Starting bot -- logging works")
 
-# ENV VARS
-load_dotenv()  # will search for .env file in local folder and load variables
-DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
-YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
-SPOTIPY_CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
-SPOTIPY_CLIENT_SECRET = os.environ["SPOTIPY_CLIENT_SECRET"]
-SPOTIPY_REDIRECT_URI = os.environ["SPOTIPY_REDIRECT_URI"]
-SPOTIFY_USERNAME= os.environ["SPOTIFY_USERNAME"]
-PLAYLIST_ID=os.environ["PLAYLIST_ID"]
-
-# discord client
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-
-# youtube client
-yt_api = Api(api_key=os.environ['YOUTUBE_API_KEY'])
-
-# Set up Spotify API
-scope = "playlist-modify-private"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-# sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'], 
-#                                                            client_secret=os.environ['SPOTIPY_CLIENT_SECRET']))
+  # will search for .env file in local folder and load variables
 
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+class Config(BaseSettings):
+    DISCORD_BOT_TOKEN: str
+    YOUTUBE_API_KEY: str
+    SPOTIPY_CLIENT_ID: str
+    SPOTIPY_CLIENT_SECRET: str
+    SPOTIPY_REDIRECT_URI: str
+    SPOTIFY_USERNAME: str
+    PLAYLIST_ID: str
+
+    class ConfigDict:
+        env_prefix = ""
 
 
-@client.event
-async def on_message(message):
-    # TODO swap in match case
-    if message.author == client.user:
-        return
-    if not message.channel.name == "music":
-        return
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
-        # print(f"message : {message.content}\nchannel: {message.channel}\n author: {message.author}\n server: {message.guild.name}")
-        logging.info("Hello message received and responsed to")
-        return
-    
-    # begin check sequences
-    is_yt, *yt_id = content_has_youtube_link(message.content)
-    if is_yt:
-        await message.channel.send(
-            """I see you posted a YouTube link. I'm currently
-                                   working on a feature to get songs into a Spotify playlist.
-                                   Stay tuned"""
+class DiscoBot():
+    # discord client
+    intents = discord.Intents.default()
+    intents.message_content = True
+    scope = "playlist-read-private"
+
+    def __init__(self):
+        self.config = Config()
+        self.client = discord.Client(intents=self.intents)
+        self.yt_api = Api(api_key=self.config.YOUTUBE_API_KEY)
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=self.config.SPOTIPY_CLIENT_ID,
+            client_secret=self.config.SPOTIPY_CLIENT_SECRET,
+            scope=self.scope)
         )
-        logging.info(
-            f"yt message : {message.content}\nchannel: {message.channel}\n author: {message.author}\n server: {message.guild.name}"
-        )
-        yt_title = get_title_from_yt_link(yt_id)
-        spotify_uri = get_spotify_uri_from_title(yt_title)
-        if spotify_uri:
-            res = add_track_to_playlist(spotify_uri)
-            if res:
-                await message.channel.send(f"Added {yt_title} to the playlist. If this is wrong. thumbs down. I'll work on removing it")
+        self.start()
+
+    def start(self):
+        bot = commands.Bot(command_prefix="!", intents=self.intents)
+        bot.add_listener(self.on_ready, "on_ready")
+        bot.add_listener(self.on_message, "on_message")
+        bot.run(self.config.DISCORD_BOT_TOKEN)
+       
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"We have logged in as {self.client.user}")
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # TODO swap in match case
+        print("message received! ", message.content)
+        if message.author == self.client.user:
+            return
+        if not message.channel.name == "music":
+            return
+        if message.content.startswith("$hello"):
+            await message.channel.send("Hello!")
+            # print(f"message : {message.content}\nchannel: {message.channel}\n author: {message.author}\n server: {message.guild.name}")
+            logging.info("Hello message received and responsed to")
+            return
+        
+        # begin check sequences
+        is_yt, *yt_id = self.content_has_youtube_link(message.content)
+        if is_yt:
+            await message.channel.send(
+                """I see you posted a YouTube link. I'm currently
+                                    working on a feature to get songs into a Spotify playlist.
+                                    Stay tuned"""
+            )
+            logging.info(
+                f"yt message : {message.content}\nchannel: {message.channel}\n author: {message.author}\n server: {message.guild.name}"
+            )
+            yt_title = self.get_title_from_yt_link(yt_id)
+            spotify_uri = self.get_spotify_uri_from_title(yt_title)
+            if spotify_uri:
+                res = self.add_track_to_playlist(spotify_uri)
+                if res:
+                    await message.channel.send(f"Added {yt_title} to the playlist. If this is wrong. thumbs down. I'll work on removing it")
+                else:
+                    await message.channel.send(f"Failed to add {yt_title} to the playlist.")
             else:
-                await message.channel.send(f"Failed to add {yt_title} to the playlist.")
+                print("No Spotify URI found for song: ", yt_title)
         else:
-            print("No Spotify URI found for song: ", yt_title)
+            print("No Action for message : ", message.content)
 
 
-    else:
-        print("No Action for message : ", message.content)
+    @staticmethod
+    def content_has_youtube_link(content: str) -> tuple[bool, str|None]:
+        # Regular expression to match YouTube URLs
+        youtube_regex = r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})'
+        youtube_match = re.search(youtube_regex, content)
+        if youtube_match:
+            video_id = youtube_match.group(6)
+            print(f"{video_id = }")
+            print("message.content has YOUTUBE: ", youtube_match.group())
+            return True, video_id
+        else:
+            return (False, None)
 
 
-def content_has_youtube_link(content: str) -> tuple[bool, str|None]:
-    # Regular expression to match YouTube URLs
-    youtube_regex = r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})'
-    youtube_match = re.search(youtube_regex, content)
-    if youtube_match:
-        video_id = youtube_match.group(6)
-        print(f"{video_id = }")
-        print("message.content has YOUTUBE: ", youtube_match.group())
-        return True, video_id
-    else:
-        (False, None)
+    def get_title_from_yt_link(self, yt_id: str) -> str:
+        """Use the yt to get the video title from the id. assume it is a song, for now"""
+        # This pastebin https://pastebin.com/Dy7eUFdS 
+        # may have a short-circuit to title and artist, if we wanna try. 
+        video = self.yt_api.get_video_by_id(video_id=yt_id)
+        return video.items[0].snippet.title
+    
+
+    def get_spotify_uri_from_title(self, yt_title: str) -> str|None:
+        results = self.sp.search(q=yt_title, type='track', limit=1)
+
+        tracks = results['tracks']['items']
+        if tracks:
+            track = tracks[0]
+            print(f"Track Name: {track['name']}, Artist: {track['artists'][0]['name']}")
+            return track['uri']
+        else:
+            print("No tracks found.")
+            return None
 
 
-def get_title_from_yt_link(yt_id: str, api=yt_api) -> str:
-    """Use the yt API to get the video title from the id. assume it is a song, for now"""
-    # This pastebin https://pastebin.com/Dy7eUFdS 
-    # may have a short-circuit to title and artist, if we wanna try. 
-    video = api.get_video_by_id(video_id=yt_id)
-    return video.items[0].snippet.title
+    def add_track_to_playlist(self, track_uri: str)-> bool:
+        # Given a validated song, add it to the playlist
+        try:
+            self.sp.playlist_add_items(self.PLAYLIST_ID, [track_uri])
+        except Exception as e:
+            print(f"Error adding track to playlist: {e}")
+            return False
+        return True
 
 
-def get_spotify_uri_from_title(yt_title: str) -> str|None:
-    results = sp.search(q=yt_title, type='track', limit=1)
-
-    tracks = results['tracks']['items']
-    if tracks:
-        track = tracks[0]
-        print(f"Track Name: {track['name']}, Artist: {track['artists'][0]['name']}")
-        return track['uri']
-    else:
-        print("No tracks found.")
-        return None
-
-
-def add_track_to_playlist(track_uri: str)-> bool:
-    # Given a validated song, add it to the playlist
-    try:
-        sp.playlist_add_items(PLAYLIST_ID, [track_uri])
-    except Exception as e:
-        print(f"Error adding track to playlist: {e}")
-        return False
-    return True
-
-
-def main(client):
-    # Suppress the default configuration since we have our own
-    client.run(token=DISCORD_BOT_TOKEN, log_handler=None)
+def main(): 
+    bot = DiscoBot()  # Bot starts() on init()
+    #bot.client.run(token=config.DISCORD_BOT_TOKEN, log_handler=None)
 
 
 if __name__ == "__main__":
-    main(client)
+    # force load .env file -- TODO can remove if dockerized and env vars are set
+    load_dotenv()
+
+    main()
--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,15 +1,40 @@
 import pytest
 
 from disco_spot.bot import (
-    get_title_from_yt_link,
-    get_spotify_uri_from_title,
-    content_has_youtube_link,
+    Config,
+    DiscoBot
 )
 
-@pytest.parametrize("text", [("https://www.youtube.com/watch?v=6JYIGclVQdw", (True, "6JYIGclVQdw"),),
-                             ("this text has no link", (False, None)),
-                             ("this text has a link https://www.youtube.com/watch?v=U7mPqycQ0tQ", (True, "U7mPqycQ0tQ"),)
-                             ])
-def test_content_has_youtube_link(text, expected):
-    res = content_has_youtube_link(text)
-    assert res == expected
+
+def test_Config_from_environment_variables(monkeypatch):
+    # Set environment variables for testing
+    monkeypatch.setenv("DISCORD_BOT_TOKEN", "your_discord_token")
+    monkeypatch.setenv("YOUTUBE_API_KEY", "your_youtube_api_key")
+    monkeypatch.setenv("SPOTIPY_CLIENT_ID", "your_spotipy_client_id")
+    monkeypatch.setenv("SPOTIPY_CLIENT_SECRET", "your_spotipy_client_secret")
+    monkeypatch.setenv("SPOTIPY_REDIRECT_URI", "your_spotipy_redirect_uri")
+    monkeypatch.setenv("SPOTIFY_USERNAME", "your_spotify_username")
+    monkeypatch.setenv("PLAYLIST_ID", "your_playlist_id")
+
+    # Create an instance of Config
+    config = Config()
+
+    # Check if values are correctly set
+    assert config.DISCORD_BOT_TOKEN == "your_discord_token"
+    assert config.YOUTUBE_API_KEY == "your_youtube_api_key"
+    assert config.SPOTIPY_CLIENT_ID == "your_spotipy_client_id"
+    assert config.SPOTIPY_CLIENT_SECRET == "your_spotipy_client_secret"
+    assert config.SPOTIPY_REDIRECT_URI == "your_spotipy_redirect_uri"
+    assert config.SPOTIFY_USERNAME == "your_spotify_username"
+    assert config.PLAYLIST_ID == "your_playlist_id"
+
+
+class TestDiscoBot:
+    @pytest.mark.parametrize("text, expected",  [
+        ("https://www.youtube.com/watch?v=6JYIGclVQdw", (True, "6JYIGclVQdw"),),
+        ("this text has no link", (False, None)),
+        ("this text has a link https://www.youtube.com/watch?v=U7mPqycQ0tQ", (True, "U7mPqycQ0tQ"),)
+    ])
+    def test_content_has_youtube_link(self, text, expected):
+        res = DiscoBot.content_has_youtube_link(text)
+        assert res == expected
